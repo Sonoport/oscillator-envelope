46d38ef70a3066c2d7ff51cb63cf1d4c9e0a58a5
Chinmay Pendharkar <notthetup@gmail.com>
2015-08-24 17:41:43 +0800

    polyphony

--- index.js
+++ index.js
@@ -4,21 +4,23 @@
 		var attackTime = 0.5;
 		var decayTime = 0.2;
 
-		var o = audioContext.createOscillator();
-		var g = audioContext.createGain();
-
-		o.connect(g);
-		g.connect(audioContext.destination);
-
-		o.start();
-		g.gain.value = 0;
-
 		document.addEventListener('keypress', function(tommy){
 			// console.log(tommy.charCode);
 			scheduleEnv(audioContext.currentTime);
 		});
 
 		function scheduleEnv(when){
+			var o = audioContext.createOscillator();
+		  var g = audioContext.createGain();
+
+		  o.connect(g);
+		  g.connect(audioContext.destination);
+
+		  o.frequency.value = Math.random()*100 + 440;
+
+		  o.start();
+		  g.gain.value = 0;
+
 			g.gain.cancelScheduledValues(when);
 			g.gain.setValueAtTime(0, when);
 			g.gain.linearRampToValueAtTime(1, when+attackTime);
